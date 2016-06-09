0ed1536ddd2abf11a79334ab22088e3d8969e791
TiagoPSS <tiagopedro96@hotmail.com>
2016-06-09 20:51:41 -0300

    [Novo] Tela AddEventoActivity ""OK"" Validacoes OK Spinner de prioridade "criado"

--- app/src/main/java/br/com/mindbit/controleacesso/gui/AddEventoActivity.java
+++ app/src/main/java/br/com/mindbit/controleacesso/gui/AddEventoActivity.java
@@ -8,85 +8,103 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 import br.com.mindbit.R;
+import br.com.mindbit.controleacesso.dominio.Evento;
+import br.com.mindbit.controleacesso.dominio.Pessoa;
+import br.com.mindbit.controleacesso.dominio.PrioridadeEvento;
 import br.com.mindbit.controleacesso.negocio.EventoNegocio;
 import br.com.mindbit.controleacesso.negocio.SessaoUsuario;
-import br.com.mindbit.controleacesso.persistencia.EventoDao;
-import br.com.mindbit.infra.gui.MindbitException;
 import br.com.mindbit.infra.gui.GuiUtil;
 
 public class AddEventoActivity extends AppCompatActivity{
 
     private Resources resources;
     private static Context contexto;
+    private EventoNegocio eventoNegocio;
+    private SessaoUsuario sessaoUsuario;
+    private Pessoa pessoaLogada;
+
+    private EditText edtEventoNome;
+    private EditText edtEventoDescricao;
+    private EditText edtEventoHoraInicio;
+    private EditText edtEventoHoraFim;
+    private EditText edtEventoDataInicio;
+    private EditText edtEventoDataFim;
+    private Button btnAdicionar;
+    private Spinner spinner;
 
     private Calendar calendar = Calendar.getInstance();
     private int year;
     private int month;
     private int day;
     private int hour;
-    private int minute1;
+    private int minute;
 
-    private EditText edtEventoNome;
-    private EditText edtEventoDescricao;
-    private EditText edtEventoHoraInicio;
-    private EditText edtEventoHoraFim ;
-    private EditText edtEventoDataInicio;
-    private EditText edtEventoDataFim;
-    private Button btnAdicionar;
+    private String nomeEvento;
+    private String descricaoEvento;
+    private String horaInicio;
+    private String horaFim;
+    private String dataInicio;
+    private String dataFim;
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    private Date format_DataInicio;
+    private Date format_DataFim;
+    private Time format_HoraInicio;
+    private Time format_HoraFim;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_evento);
 
+        resources = getResources();
+
         contexto = this;
 
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-        hour = calendar.get(Calendar.HOUR_OF_DAY);
-        minute1 = calendar.get(Calendar.MINUTE);
-        btnAdicionar = (Button) findViewById(R.id.btnAdicionar);
-        edtEventoNome = (EditText) findViewById(R.id.edtEventoNome);
-        edtEventoDescricao = (EditText) findViewById(R.id.edtEventoDescricao);
-        edtEventoHoraInicio = (EditText) findViewById(R.id.edtEventoHoraInicio);
-        edtEventoHoraFim = (EditText) findViewById(R.id.edtEventoHoraFim);
-        edtEventoDataInicio = (EditText) findViewById(R.id.edtEventoDataInicio);
-        edtEventoDataFim = (EditText) findViewById(R.id.edtEventoDataFim);
-        edtEventoDataInicio.setFocusable(false);
-        edtEventoDataFim.setFocusable(false);
-        edtEventoHoraInicio.setFocusable(false);
-        edtEventoHoraFim.setFocusable(false);
-
-
-        btnAdicionar.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                cadastrarEvento();
-
-            }
-        });
+        this.setCamposAddEvento();
+        eventoNegocio = EventoNegocio.getInstancia(contexto);
+        sessaoUsuario = SessaoUsuario.getInstancia();
+        pessoaLogada = sessaoUsuario.getPessoaLogada();
 
         edtEventoHoraFim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                TimePickerDialog timepick = new TimePickerDialog(AddEventoActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                final TimePickerDialog timepick = new TimePickerDialog(AddEventoActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        String strHoraFim = hourOfDay+":"+minute;
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                        Date data = null;
+                        try {
+                            data = simpleDateFormat.parse(strHoraFim);
+                            Time timeHoraFim = new Time(data.getTime());
+                            format_HoraFim = timeHoraFim;
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+
                         edtEventoHoraFim.setText(hourOfDay + ":" + minute);
+
                     }
-                }, hour, minute1, true
+                }, hour, minute, true
                 );
                 timepick.show();
             }
@@ -99,9 +117,20 @@ public class AddEventoActivity extends AppCompatActivity{
                 TimePickerDialog timepick = new TimePickerDialog(AddEventoActivity.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                        String strHoraInicio = hourOfDay+":"+minute;
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                        Date data = null;
+                        try {
+                            data = simpleDateFormat.parse(strHoraInicio);
+                            Time timeHoraInicio = new Time(data.getTime());
+                            format_HoraInicio = timeHoraInicio;
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+
                         edtEventoHoraInicio.setText(hourOfDay + ":" + minute);
                     }
-                }, hour, minute1, true
+                }, hour, minute, true
                 );
                 timepick.show();
             }
@@ -110,68 +139,135 @@ public class AddEventoActivity extends AppCompatActivity{
         edtEventoDataInicio.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datepicker = new DatePickerDialog(AddEventoActivity.this, new DatePickerDialog.OnDateSetListener(){
+                DatePickerDialog datepicker = new DatePickerDialog(AddEventoActivity.this, new DatePickerDialog.OnDateSetListener() {
 
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+
                         int mesCerto = monthOfYear + 1;
+
+                        Calendar data = Calendar.getInstance();
+                        data.set(year, mesCerto, dayOfMonth);
+                        format_DataInicio = data.getTime();
+
                         edtEventoDataInicio.setText(dayOfMonth + "/" + mesCerto + "/" + year);
                     }
-                },year,month,day);
+                }, year, month, day);
                 datepicker.show();
             }
         });
         edtEventoDataFim.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatePickerDialog datepicker = new DatePickerDialog(AddEventoActivity.this, new DatePickerDialog.OnDateSetListener(){
+                DatePickerDialog datepicker = new DatePickerDialog(AddEventoActivity.this, new DatePickerDialog.OnDateSetListener() {
 
                     @Override
                     public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                         int mesCerto = monthOfYear + 1;
+
+                        Calendar data = Calendar.getInstance();
+                        data.set(year, mesCerto, dayOfMonth);
+                        format_DataFim = data.getTime();
+
                         edtEventoDataFim.setText(dayOfMonth + "/" + mesCerto + "/" + year);
                     }
-                },year,month,day);
+                }, year, month, day);
                 datepicker.show();
             }
         });
 
-    }
-    private boolean validateFieldsEvento() {
-        String nome = edtEventoNome.getText().toString().trim();
-        String descricao = edtEventoDescricao.getText().toString().trim();
-        String dataInicio = edtEventoDataInicio.getText().toString().trim();
-        String dataFim = edtEventoDataFim.getText().toString().trim();
-        String horaInicio = edtEventoDataInicio.getText().toString().trim();
-        String horaFim = edtEventoDataInicio.getText().toString().trim();
+        switch (spinner.getSelectedItemPosition()){
+            case 0:
+                GuiUtil.exibirMsg(this, "SETAR prio VERDE");
+                break;
+            case 1:
+                GuiUtil.exibirMsg(this, "SETAR prio AMARELA");
+                break;
+            case 2:
+                GuiUtil.exibirMsg(this, "SETAR prio VERMELHA");
+                break;
+        }
 
-        return (!isEmptyFields(nome, descricao, dataInicio, dataFim, horaInicio, horaFim)
-                && hasSizeValid(nome, descricao) && rangeTimeValid(dataInicio, dataFim));
+
+        btnAdicionar.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                    cadastrarEvento();
+
+            }
+        });
     }
 
-    private boolean isEmptyFields(String nome, String descr, String dataIn,
-                                  String dataFi, String horaIn, String horaFi) {
+    private void setCamposAddEvento(){
+
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        hour = calendar.get(Calendar.HOUR_OF_DAY);
+        minute = calendar.get(Calendar.MINUTE);
+
+        Spinner spinner = (Spinner) findViewById(R.id.prioridade_spinner);
+        spinner.setAdapter(new ArrayAdapter<PrioridadeEvento>(this, android.R.layout.simple_list_item_1, PrioridadeEvento.values()));
+        btnAdicionar = (Button) findViewById(R.id.btnAdicionar);
+
+        edtEventoNome = (EditText) findViewById(R.id.input_edtEventoNome);
+        edtEventoDescricao = (EditText) findViewById(R.id.input_edtEventoDescricao);
+        edtEventoHoraInicio = (EditText) findViewById(R.id.input_edtEventoHoraInicio);
+        edtEventoHoraFim = (EditText) findViewById(R.id.input_edtEventoHoraFim);
+        edtEventoDataInicio = (EditText) findViewById(R.id.input_edtEventoDataInicio);
+        edtEventoDataFim = (EditText) findViewById(R.id.input_edtEventoDataFim);
+        edtEventoDataInicio.setFocusable(false);
+        edtEventoDataFim.setFocusable(false);
+        edtEventoHoraInicio.setFocusable(false);
+        edtEventoHoraFim.setFocusable(false);
+    }
+
+
+    private boolean validateFieldsEvento()  {
+
+        nomeEvento = edtEventoNome.getText().toString().trim();
+        descricaoEvento = edtEventoDescricao.getText().toString().trim();
+        dataInicio = edtEventoDataInicio.getText().toString().trim();
+        dataFim = edtEventoDataFim.getText().toString().trim();
+        horaInicio = edtEventoDataInicio.getText().toString().trim();
+        horaFim = edtEventoDataInicio.getText().toString().trim();
+
+        //format_DataInicio = formatarData(dataInicio);
+        //format_DataFim = formatarData(dataFim);
+        //format_HoraInicio = formatarHora(horaInicio);
+        //format_HoraFim = formatarHora(horaFim);
+
+        return (!isEmptyFields(nomeEvento, descricaoEvento, dataInicio, dataFim, horaInicio, horaFim)
+                && hasSizeValid(nomeEvento, descricaoEvento) && rangeDateValid(format_DataInicio,format_DataFim));
+
+    }
+
+    private boolean isEmptyFields(String nome, String descricao, String dataInicio,
+                                  String dataFim, String horaInicio, String horaFim) {
         if (TextUtils.isEmpty(nome)) {
             edtEventoNome.requestFocus();
             edtEventoNome.setError(resources.getString(R.string.signUp_name_required));
             return true;
-        } else if (TextUtils.isEmpty(descr)) {
+        } else if (TextUtils.isEmpty(descricao)) {
             edtEventoDescricao.requestFocus();
             edtEventoDescricao.setError(resources.getString(R.string.addEvento_edt_descricao));
             return true;
-        }else if (TextUtils.isEmpty(dataIn)) {
+        }else if (TextUtils.isEmpty(dataInicio)) {
             edtEventoDataInicio.requestFocus();
             edtEventoDataInicio.setError(resources.getString(R.string.addEvento_edt_data_inicial));
             return true;
-        }else if (TextUtils.isEmpty(dataFi)) {
+        }else if (TextUtils.isEmpty(dataFim)) {
             edtEventoDataFim.requestFocus();
             edtEventoDataFim.setError(resources.getString(R.string.addEvento_edt_data_final));
             return true;
-        }else if (TextUtils.isEmpty(horaIn)) {
+        }else if (TextUtils.isEmpty(horaInicio)) {
             edtEventoHoraInicio.requestFocus();
             edtEventoHoraInicio.setError(resources.getString(R.string.addEvento_edt_hora_inicial));
             return true;
-        }else if (TextUtils.isEmpty(horaFi)) {
+        }else if (TextUtils.isEmpty(horaFim)) {
             edtEventoHoraFim.requestFocus();
             edtEventoHoraFim.setError(resources.getString(R.string.addEvento_edt_hota_final));
             return true;
@@ -180,50 +276,62 @@ public class AddEventoActivity extends AppCompatActivity{
     }
 
     private boolean hasSizeValid(String nome, String descricao) {
-        if (!(nome.length() > 10)) {
+        if (!(nome.length() > 4)) {
             edtEventoNome.requestFocus();
             edtEventoNome.setError(resources.getString(R.string.login_user_invalid));
             return false;
-        } else if (!(descricao.length() > 10)) {
+        } else if (!(descricao.length() > 4)) {
             edtEventoDescricao.requestFocus();
-            edtEventoDescricao.setError(resources.getString(R.string.login_password_invalid));
+            edtEventoDescricao.setError(resources.getString(R.string.addEvento_edt_small_description));
             return false;
         }
         return true;
     }
-    private boolean rangeTimeValid(String dataInicio, String dataFim){
-        String[] partsInicio = dataInicio.split("/");
-        String[] partsFim = dataFim.split("/");
-        int anoInicio = Integer.parseInt(partsInicio[2]);
-        int anoFim = Integer.parseInt(partsFim[2]);
-        int mesInicio = Integer.parseInt(partsInicio[1]);
-        int mesFim = Integer.parseInt(partsFim[1]);
-        int diaInicio = Integer.parseInt(partsInicio[0]);
-        int diaFim = Integer.parseInt(partsFim[0]);
-        if (anoInicio <= anoFim){
-            if (mesInicio <= mesFim){
-                if (diaInicio <= diaFim){
-                    return true;
-                }else { return false; }
-            }else { return false; }
+    private boolean rangeDateValid(Date date1, Date date2) {
 
-        }else { return false; }
-
+        if(date1.compareTo(date2) == 1){
+            GuiUtil.exibirMsg(this,resources.getString(R.string.addEvento_edt_data_hora_erro));
+            return false;
+        }
+        return true;
     }
 
-    private SessaoUsuario sessaoUsuario = SessaoUsuario.getInstancia();
-    private EventoNegocio eventoNegocio = EventoNegocio.getInstancia(contexto);
-    private  void cadastrarEvento(){
-        if(validateFieldsEvento()){
-            int idUsuarioLogado = sessaoUsuario.getUsuarioLogado().getId();
-            //eventoNegocio.validarCadastroEvento();
 
-            /*try {
+
+    private  void cadastrarEvento(){
+        if(validateFieldsEvento()) {
+            Toast.makeText(this,"validado",Toast.LENGTH_LONG).show();
+        }
+            /*try{
+
+                int idUsuarioLogado = sessaoUsuario.getUsuarioLogado().getId();
+                Evento evento = new Evento();
+                evento.setId(idUsuarioLogado);
+                evento.setNome(nomeEvento);
+                evento.setDescricao(descricaoEvento);
+                evento.setDataInicio(format_DataInicio);
+                evento.setDataFim(format_DataFim);
+                evento.setHoraInicio(format_HoraInicio);
+                evento.setHoraFim(format_HoraFim);
+                eventoNegocio.validarCadastroEvento(evento);
+
+
             }catch (MindbitException e){
                 GuiUtil.exibirMsg(AddEventoActivity.this, e.getMessage());
 
             }*/
-        }
+            int idUsuarioLogado = sessaoUsuario.getUsuarioLogado().getId();
+            Evento evento = new Evento();
+            evento.setId(idUsuarioLogado);
+            evento.setNome(nomeEvento);
+            evento.setDescricao(descricaoEvento);
+            evento.setDataInicio(format_DataInicio);
+            evento.setDataFim(format_DataFim);
+            evento.setHoraInicio(format_HoraInicio);
+            evento.setHoraFim(format_HoraFim);
+            //eventoNegocio.validarCadastroEvento(evento);}
     }
+
+
 }
 
