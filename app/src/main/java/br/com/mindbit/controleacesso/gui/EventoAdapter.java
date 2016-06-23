67051069bd0c7f9d4a25aba1d1ddef16acc7675f
Victor Leuthier <keko_leuthier@hotmail.com>
2016-06-23 18:57:16 -0300

    [Ajuste] Layout de list_item e pesquisar_evento [Criacao] Metodo para exibir a foto de acordo com a prioridade

--- app/src/main/java/br/com/mindbit/controleacesso/gui/EventoAdapter.java
+++ app/src/main/java/br/com/mindbit/controleacesso/gui/EventoAdapter.java
@@ -1,15 +1,12 @@
 package br.com.mindbit.controleacesso.gui;
 
 import android.content.Context;
-import android.media.Image;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -18,28 +15,21 @@ import java.util.List;
 
 import br.com.mindbit.R;
 import br.com.mindbit.controleacesso.dominio.Evento;
-import br.com.mindbit.controleacesso.dominio.PrioridadeEvento;
-import br.com.mindbit.controleacesso.persistencia.EventoDao;
-import br.com.mindbit.infra.gui.GuiUtil;
 
 public class EventoAdapter extends BaseAdapter {
         private LayoutInflater mInflater;
-        private List<Evento> itens;
-        private PrioridadeEvento prioridadeEvento;
-        private EventoDao eventoDao;
+        private List<Evento> eventos;
         private Evento evento;
 
-        public EventoAdapter(Context context, List<Evento> itens)
-        {
-            this.itens = itens;
+        public EventoAdapter(Context context, List<Evento> eventos){
+            this.eventos = eventos;
 
             mInflater = LayoutInflater.from(context);
         }
 
-        public int getCount()
-        {
-            if (itens != null) {
-                return itens.size();
+        public int getCount(){
+            if (eventos != null) {
+                return eventos.size();
             } else {
                 return 0;
             }
@@ -47,7 +37,7 @@ public class EventoAdapter extends BaseAdapter {
 
         public Evento getItem(int posicao)
         {
-            return itens.get(posicao);
+            return eventos.get(posicao);
         }
 
         public long getItemId(int posicao)
@@ -55,28 +45,24 @@ public class EventoAdapter extends BaseAdapter {
             return posicao;
         }
 
-        public View getView(int posicao, View view, ViewGroup parent)
-        {
+        public View getView(int posicao,View view, ViewGroup parent){
 
-            Evento item = itens.get(posicao);
-            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+            evento = eventos.get(posicao);
+
             view = mInflater.inflate(R.layout.list_item_pesquisar_evento, null);
-            ((TextView) view.findViewById(R.id.txtitem_nome_evento)).setText(item.getNome());
-            ((TextView) view.findViewById(R.id.txtitem_descricao_evento)).setText(item.getDescricao());
-            String inicio = simpleDateFormat.format(addOneMonth(item.getDataInicio()));
+
+            ((TextView) view.findViewById(R.id.txtitem_nome_evento)).setText(evento.getNome());
+            ((TextView) view.findViewById(R.id.txtitem_descricao_evento)).setText(evento.getDescricao());
+
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+            String inicio = simpleDateFormat.format(addOneMonth(evento.getDataInicio()));
             ((TextView) view.findViewById(R.id.txtitem_data_evento)).setText(inicio);
-            if (item.getNivelPrioridadeEnum().ordinal() == 0 ) {
-                ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_verde);
-                return view;
-            }else if (item.getNivelPrioridadeEnum().ordinal() == 1) {
-                ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_amarela);
-                return view;
-            }else if (item.getNivelPrioridadeEnum().ordinal() == 2) {
-                ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_vermelha);
-                return view;
-            }
+
+            setIconePrioridade(view);
+
             return view;
         }
+
     public static Date addOneMonth(Date date)
     {
         Calendar cal = Calendar.getInstance();
@@ -84,4 +70,20 @@ public class EventoAdapter extends BaseAdapter {
         cal.add(Calendar.MONTH, 1);
         return cal.getTime();
     }
+
+    public View setIconePrioridade(View view){
+        int nivelPrioridade = evento.getNivelPrioridadeEnum().ordinal();
+
+        if (nivelPrioridade == 0 ) {
+            ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_verde);
+            return view;
+        }else if (nivelPrioridade == 1) {
+            ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_amarela);
+            return view;
+        }else if (nivelPrioridade == 2) {
+            ((ImageView) view.findViewById(R.id.img_evento)).setImageResource(R.drawable.bola_vermelha);
+            return view;
+        }return view;
     }
+
+}
