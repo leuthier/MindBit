c97ca449b07d7838d1f980eb159089736957e0da
Ariana Lima <ariana.hsm@gmail.com>
2016-06-28 17:21:59 -0300

    compartilhar evento em andamento

--- app/src/main/java/br/com/mindbit/controleacesso/gui/CompartilharEventoActivity.java
+++ app/src/main/java/br/com/mindbit/controleacesso/gui/CompartilharEventoActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,8 +12,11 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import br.com.mindbit.R;
@@ -32,14 +34,20 @@ public class CompartilharEventoActivity extends AppCompatActivity{
     private SessaoUsuario sessaoUsuario;
     private Pessoa pessoaLogada;
     private ArrayList<Evento> eventosPessoa;
+    private List<Pessoa> pessoasDestinatarios;
+    private List<Evento> eventosMarcardos;
 
     private ListView listaEventos;
     private Button btnCompartilhar;
+    private ArrayAdapter<String> adapter = null;
+    ArrayList<String> list = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compartilhar_evento);
+        //Bundle bundle = getIntent().getExtras();
 
         resources = getResources();
 
@@ -58,41 +66,59 @@ public class CompartilharEventoActivity extends AppCompatActivity{
         });
 
 
-        ArrayList<String> list = new ArrayList<String>();
-
-        try {
-            initList();
-            for (Evento evento:initList()) {
-                list.add(evento.getNome()+" -> " +
-                        "\n"+evento.getDataInicio().getDate()+"/"+evento.getDataInicio().getMonth()+"/"+evento.getDataInicio().getYear()+
-                        " às "+evento.getDataInicio().getHours()+":"+evento.getDataInicio().getMinutes()+
-                        " até \n"+evento.getDataFim().getDate()+"/"+evento.getDataFim().getMonth()+"/"+evento.getDataFim().getYear()+
-                        " às "+evento.getDataFim().getHours()+":"+evento.getDataFim().getMinutes());
-
-            }
-        } catch (MindbitException e) {
-            GuiUtil.exibirMsg(CompartilharEventoActivity.this, e.getMessage());
-        }
-
-
-        if (listaEventos!=null) {
-            ArrayAdapter<String> adapter = new ContactListAdapter(this, list);
-            listaEventos.setAdapter(adapter);
-        }
+       mostrarLista();
     }
 
-    public List<Evento> initList() throws MindbitException{
-        eventosPessoa = eventoNegocio.listarEventosProximo(pessoaLogada.getId());
-        return eventosPessoa;
-    }
 
     public void compartilhar(View v){
-        Intent i = new Intent(Intent.ACTION_SEND);
-        i.putExtra(Intent.EXTRA_EMAIL, new String[] {"amiguinho@email.com","amigo@email.com"} );
-        i.putExtra(Intent.EXTRA_SUBJECT, "Evento...");
-        i.putExtra(Intent.EXTRA_TEXT, "Aqui vem o texto da mensagem");
 
-        startActivity(Intent.createChooser(i, "Compartilhar para:"));
+        String[] emailsDestino = new String[]{getDestinariosEmails(pessoasDestinatarios)};
+        StringBuilder conteudoEmail = getEmailConteudo(eventosMarcardos);
+        //ArrayList<String> emailConteudo = getEmailConteudo(eventosMarcardos);
+        String subject = ("MindBit - Alguém compartilhou eventos com você!");
+
+
+        checkButtonClick();
+        if (checkButtonClick()!=null) {
+            Intent i = new Intent(Intent.ACTION_SEND);
+            i.setType("text/plain");
+            i.putExtra(android.content.Intent.EXTRA_EMAIL, emailsDestino);
+            i.putExtra(Intent.EXTRA_SUBJECT, subject);
+            i.putExtra(Intent.EXTRA_TEXT, conteudoEmail.toString());
+            startActivity(Intent.createChooser(i, "Compartilhar para:"));
+        }else{
+            GuiUtil.exibirMsg(this,"Selecione algum evento");
+        }
+    }
+
+    public String getDestinariosEmails(List<Pessoa> destinatarios){
+        String emailsDestino = new String();
+
+/*        for (Pessoa pessoa : destinatarios) {
+            emailsDestino += pessoa.getEmail()+",";
+
+        }*/
+
+        emailsDestino+="ariana@teste.com"+","+"victor.leuthier@ufrpe.br"+",";
+
+        return emailsDestino;
+    }
+
+    public StringBuilder getEmailConteudo(List<Evento> eventos){
+        StringBuilder infoEventos = new StringBuilder();
+
+ /*       for (Evento evento : eventos) {
+            infoEventos += evento.getNome() + " -> " +
+                    "\n" + evento.getDataInicio().getDate() + "/" + evento.getDataInicio().getMonth() + "/" + evento.getDataInicio().getYear() +
+                    " às " + evento.getDataInicio().getHours() + ":" + evento.getDataInicio().getMinutes() +
+                    " até \n" + evento.getDataFim().getDate() + "/" + evento.getDataFim().getMonth() + "/" + evento.getDataFim().getYear() +
+                    " às " + evento.getDataFim().getHours() + ":" + evento.getDataFim().getMinutes());
+        }
+*/
+
+        infoEventos.append("Prova economia dia30 hora34\n"+"Apresentaçao mpoo dia30 hora90\n");
+       // infoEventos+= "\nAtenciosamente, " + "\n" + pessoaLogada.getNome()+".";
+        return infoEventos;
     }
 
     private class ContactListAdapter extends ArrayAdapter<String> {
@@ -112,6 +138,55 @@ public class CompartilharEventoActivity extends AppCompatActivity{
             name.setText(s);
 
             return view;
+        }
+    }
+
+    private StringBuilder checkButtonClick() {
+        StringBuilder mensagem = new StringBuilder();
+        btnCompartilhar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer responseText = new StringBuffer();
+                responseText.append("The following were selected...\n");
+
+                // ArrayList<String> countryList = adapter.countryList;
+                for (int i = 0; i < list.size(); i++) {
+                    String evento = list.get(i);
+                    //if(evento.isSelected()){
+                    responseText.append("\n" + evento);
+                    //}
+                }
+
+                Toast.makeText(getApplicationContext(),
+                        responseText, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+     return mensagem;
+    }
+
+    public void mostrarLista(){
+        ArrayList<String> list = new ArrayList<String>();
+
+        try {
+            eventosPessoa = eventoNegocio.listarEventosProximo(pessoaLogada.getId());
+            for (Evento evento:eventosPessoa) {
+                list.add(evento.getNome()+" -> " +
+                        "\n"+evento.getDataInicio().getDate()+"/"+evento.getDataInicio().getMonth()+"/"+evento.getDataInicio().getYear()+
+                        " às "+evento.getDataInicio().getHours()+":"+evento.getDataInicio().getMinutes()+
+                        " até \n"+evento.getDataFim().getDate()+"/"+evento.getDataFim().getMonth()+"/"+evento.getDataFim().getYear()+
+                        " às "+evento.getDataFim().getHours()+":"+evento.getDataFim().getMinutes());
+
+            }
+        } catch (MindbitException e) {
+            GuiUtil.exibirMsg(CompartilharEventoActivity.this, e.getMessage());
+        }
+
+        if (listaEventos!=null) {
+            ArrayAdapter<String> adapter = new ContactListAdapter(this, list);
+            listaEventos.setAdapter(adapter);
         }
     }
 }
